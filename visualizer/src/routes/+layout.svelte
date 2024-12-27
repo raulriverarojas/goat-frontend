06a5c8c15ace989c885df7ee1973b83e74a36bc5
Raul <rrdarior@gmail.com>
2024-12-27 12:59:17 -0500

    switched navbar to non fixed on desktop

--- visualizer/src/routes/+layout.svelte
+++ visualizer/src/routes/+layout.svelte
@@ -14,7 +14,7 @@
 </script>
 <!-- NavBar.svelte -->
 {#if isMobile}
-  <div class="navbar fixed top-5 left-0 right-0-center z-10 mb-4">
+  <div class="navbar fixed top-5 left-0 z-10 ">
     <div class="navbar-start">
       <div class="dropdown ">
         <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
@@ -67,39 +67,39 @@
     </div>
   </div>
 {:else}
-<div class="navbar fixed top-5 left-0 right-0 flex justify-center z-10 mb-30">
-    <ul class="menu bg-base-300 lg:menu-horizontal rounded-box">
-        <li>
-            <a href="/">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                    />
-                </svg>
-                Home
-            </a>
-        </li>
-        <li>
-            <a href="/login">
-                Login
-            </a>
-        </li>
-        <li>
-            <a href="/register">
-                Register
-                <span class="badge badge-xs badge-info"></span>
-            </a>
-        </li>
-    </ul>
-</div>
+	<div class="navbar top-5 left-0 right-0 flex justify-center z-10 mb-30">
+		<ul class="menu bg-base-300 lg:menu-horizontal rounded-box">
+			<li>
+				<a href="/">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+						/>
+					</svg>
+					Home
+				</a>
+			</li>
+			<li>
+				<a href="/login">
+					Login
+				</a>
+			</li>
+			<li>
+				<a href="/register">
+					Register
+					<span class="badge badge-xs badge-info"></span>
+				</a>
+			</li>
+		</ul>
+	</div>
 {/if}
 {@render children()}
